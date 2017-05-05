9131d2716eb1475234e3b62a0d4a0af9b02979c1
huynh <huynhduc96@gmail.com>
2017-05-05 16:08:21 +0700

    update json grass

--- src/base/jsonObject/JoGrass.java
+++ src/base/jsonObject/JoGrass.java
@@ -8,20 +8,16 @@ package base.jsonObject;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class JoGrass {
+import java.util.List;
+
+public class JoGrass  {
 
     @SerializedName("total_number")
     @Expose
     private String totalNumber;
-    @SerializedName("step1")
+    @SerializedName("list_grass")
     @Expose
-    private String step1;
-    @SerializedName("step2")
-    @Expose
-    private String step2;
-    @SerializedName("step3")
-    @Expose
-    private String step3;
+    private List<ListGras> listGrass = null;
 
     public String getTotalNumber() {
         return totalNumber;
@@ -31,28 +27,12 @@ public class JoGrass {
         this.totalNumber = totalNumber;
     }
 
-    public String getStep1() {
-        return step1;
+    public List<ListGras> getListGrass() {
+        return listGrass;
     }
 
-    public void setStep1(String step1) {
-        this.step1 = step1;
-    }
-
-    public String getStep2() {
-        return step2;
-    }
-
-    public void setStep2(String step2) {
-        this.step2 = step2;
-    }
-
-    public String getStep3() {
-        return step3;
-    }
-
-    public void setStep3(String step3) {
-        this.step3 = step3;
+    public void setListGrass(List<ListGras> listGrass) {
+        this.listGrass = listGrass;
     }
 
 }
